1ce2ea8e56c2f5e0b02c9d6b6b3315e929f5702e
João Pucci <joao.pucci@live.com>
2020-10-04 03:42:02 -0300

    v0.0.2-alpha
    * Create spotlight + cash view
    
    * Add products list
    
    * Add translation
    
    * Add loading
    
    * Add Viewmodel tests
    
    * Add github actions for tests
    
    * Change branch for tests
    
    * Create README.md
    
    * Add error placeholder

--- app/src/main/java/com/example/listexample/presenter/binding/BindingAdapter.kt
+++ app/src/main/java/com/example/listexample/presenter/binding/BindingAdapter.kt
@@ -2,17 +2,29 @@ package com.example.listexample.presenter.binding
 
 import android.view.View
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.example.listexample.R
 import com.squareup.picasso.Picasso
 
 @BindingAdapter("imageUrl")
 fun setImageUrl(imageView: ImageView, url: String?) {
     url?.let {
-        if (it.isNotBlank()) Picasso.get().load(it).into(imageView)
+        if (it.isNotBlank()) loadImage(it, imageView)
+    }
+}
+
+private fun loadImage(url: String, imageView: ImageView) {
+    with(Picasso.get().load(url)) {
+        ContextCompat.getDrawable(imageView.context, R.drawable.placeholder_item_product)
+            ?.let {
+                error(it)
+            }
+        into(imageView)
     }
 }
 
 @BindingAdapter("isVisible")
 fun setIsVisible(view: View, isVisible: Boolean) {
-    view.visibility  = if(isVisible) View.VISIBLE else View.INVISIBLE
+    view.visibility = if (isVisible) View.VISIBLE else View.INVISIBLE
 }
